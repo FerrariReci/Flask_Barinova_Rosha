2a6087ae1f8f64a5ddd947ed6fb13bbc90f2f5d7
123 <arsseni1997up@gmail.com>
2024-04-29 16:25:58 +0300

    Все файлы по сайту + бд + css, без ком.

---
+++ forms/user.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Электронная почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    username = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
